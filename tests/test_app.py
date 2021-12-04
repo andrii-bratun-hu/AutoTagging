7d3b5390b89775dae5a33dc113a320bc66bcbde9
Andrii Bratun <andrii.bratun@health-union.com>
2021-12-05 01:50:02 +0200

    change tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,5 @@
 import warnings
 import snowflake.connector as sf
-from snowflake.connector import DictCursor
 from configs.credentials import snowflake_login
 
 from fastapi.testclient import TestClient
@@ -19,12 +18,10 @@ def test_snowflake_connection():
     except:
         assert False
 
-
 def test_ping():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == 'pong'
-
 
 def test_bertopic_keywords_number_less_than_zero():
     response = client.post("/api/bertopic", json={"keywords_number": -1, "post_ids": ["99918_WP", "99917_WP"], "posts_content": ["As a big sister to four very sweet, hilarious, and very active younger brothers", "If there was one thing you learned by a young age"]})
@@ -39,10 +36,11 @@ def test_bertopic_keywords_number_must_be_int_if_grater_than_one():
 def test_bertopic_keywords_sample_request():
     warnings.filterwarnings("ignore")
     response = client.post("/api/bertopic", json={"keywords_number": 2, "post_ids": ["99918_WP", "99917_WP"], "posts_content": ["As a big sister to four very sweet, hilarious, and very active younger brothers", "If there was one thing you learned by a young age"]})
-    print(response.json())
+    response_json = response.json()
     assert response.status_code == 200
-    assert response.json() == {"post_ids":["99918_WP","99917_WP"],"keywords":[["pain","day"],["pain","day"]]}
-
+    assert len(response_json['keywords']) == 2
+    assert len(response_json['keywords'][0]) == 2
+    assert len(response_json['keywords'][1]) == 2
 
 def test_keybert_keywords_number_less_than_zero():
     response = client.post("/api/autokeyword", json={"keywords_number": -1, "post_ids": ["99918_WP", "99917_WP"], "posts_content": ["As a big sister to four very sweet, hilarious, and very active younger brothers", "If there was one thing you learned by a young age"]})
@@ -57,5 +55,8 @@ def test_keybert_keywords_number_must_be_int_if_grater_than_one():
 def test_keybert_keywords_sample_request():
     warnings.filterwarnings("ignore")
     response = client.post("/api/autokeyword", json={"keywords_number": 2, "post_ids": ["99918_WP", "99917_WP"], "posts_content": ["As a big sister to four very sweet, hilarious, and very active younger brothers", "If there was one thing you learned by a young age"]})
+    response_json = response.json()
     assert response.status_code == 200
-    assert response.json() == {"post_ids":["99918_WP","99917_WP"],"keywords":[["sister sweet hilarious","active younger brothers"],["learned young age","thing"]]}
+    assert len(response_json['keywords']) == 2
+    assert len(response_json['keywords'][0]) == 2
+    assert len(response_json['keywords'][1]) == 2
